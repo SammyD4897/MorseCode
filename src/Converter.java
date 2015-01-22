730d12a1287729570b0549b611d79b1339269b5b
SammyD4897 <shadowslamer@gmail.com>
2015-01-21 19:55:24 -0600

    Work

--- src/Converter.java
+++ src/Converter.java
@@ -25,9 +25,9 @@ public class Converter {
 	public static String convert(String letter){
 		String[] letters  = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
 		String[] codes = {"+-", "-+++", "-+-+", "-++", "+", "++-+", "--+", "++++", "++", "+---", "-+-", "+-++", "--", "-+", "---", "+--+", "--+-", "+-+", "+++", "-", "++-", "+++-", "+--", "-++-", "-+--", "--++", "-----", "+----", "++---", "+++--", "++++-", "+++++", "-++++", "--+++", "---++", "----+"};
-		
 		int location = Arrays.binarySearch(letters, letter);
 		String morse = codes[location];
+		
 		return morse;
 	}
 
