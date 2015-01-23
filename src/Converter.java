19d59de92b891e3bd56ebeae24c84035726e9d21
SammyD4897 <shadowslamer@gmail.com>
2015-01-22 19:11:06 -0600

    Fixed errors

--- src/Converter.java
+++ src/Converter.java
@@ -6,25 +6,26 @@ public class Converter {
 		Scanner s = new Scanner(System.in);
 		String word = "";
 		while(!word.equals("!")){
-		System.out.print("Please enter a word: ");
-		word = s.next();
-		String output = "";
-		int wLen = word.length();
-		System.out.print("|");
-		for (int i = 0; i<wLen; i++){
-			char letter = word.charAt(i);
-			String sLetter = Character.toString(letter);
-			String code = convert(sLetter);
-			//output+= code;
-			//output+="|";
-			System.out.print(code+"|");
+			System.out.print("Please enter a word: ");
+			word = s.next();
+			String output = "";
+			int wLen = word.length();
+			System.out.print("|");
+			for (int i = 0; i<wLen; i++){
+				char letter = word.charAt(i);
+				String sLetter = Character.toString(letter);
+				String code = convert(sLetter);
+				//output+= code;
+				//output+="|";
+				System.out.print(code+"|");
+			}
+			System.out.println("");
 		}
-		System.out.println("");
-		}
+		s.close();
 	}
 	public static String convert(String letter){
-		String[] letters  = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
-		String[] codes = {"+-", "-+++", "-+-+", "-++", "+", "++-+", "--+", "++++", "++", "+---", "-+-", "+-++", "--", "-+", "---", "+--+", "--+-", "+-+", "+++", "-", "++-", "+++-", "+--", "-++-", "-+--", "--++", "-----", "+----", "++---", "+++--", "++++-", "+++++", "-++++", "--+++", "---++", "----+"};
+		String[] letters  = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+		String[] codes = {"-----", "+----", "++---", "+++--", "++++-", "+++++", "-++++", "--+++", "---++", "----+", "+-", "-+++", "-+-+", "-++", "+", "++-+", "--+", "++++", "++", "+---", "-+-", "+-++", "--", "-+", "---", "+--+", "--+-", "+-+", "+++", "-", "++-", "+++-", "+--", "-++-", "-+--", "--++"};
 		int location = Arrays.binarySearch(letters, letter);
 		String morse = codes[location];
 		
